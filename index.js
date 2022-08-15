a28eaabac04b68e7679a02e3aecaaac81f3bd540
Annciatar Ndumi <annciatar.kavita@student.moringaschool.com>
2022-08-15 08:49:02 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
 
 // Write your code here
+const num1 = 31;
+const num2 = 2;
+const multiply = 31*2;
+
+
+const random = 10;
+Math.floor(Math.random() *  10) + 1;
+
+const num3 = 14;
+const num4 = 5;
+const mod = 4;
+14 % 5;
+
+let max = 10;
+max += 10;
